c1face22f715dcd448043c36e8bcd5aa9b6eb8d9
Chitransh S <chitransh0210@gmail.com>
2024-05-15 14:07:09 +0530

    Updating image path

--- src/RobotDetails.jsx
+++ src/RobotDetails.jsx
@@ -1,50 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-  Box,
-  Container,
-  Heading,
-  SimpleGrid,
-  Card,
-  CardBody,
-  Image,
-  Spinner,
-  Alert,
-  AlertIcon,
-  Text,
+import { 
+  Box, 
+  Container, 
+  Heading, 
+  SimpleGrid, 
+  Card, 
+  CardBody, 
+  Image, 
+  Spinner, 
+  Alert, 
+  AlertIcon, 
+  Text, 
   Flex,
   useColorModeValue,
   ScaleFade,
   Badge,
   Center
 } from '@chakra-ui/react';
-import './RobotDetails.css';  // Assuming you will add some global styles here
+import './RobotDetails.css';  
 
 const RobotDetails = ({ robotName }) => {
   const [robotData, setRobotData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [imageLoaded, setImageLoaded] = useState(false);
   const [error, setError] = useState(null);
   const bgColor = useColorModeValue('gray.100', 'gray.700');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true); // Ensure loading is true at the start of data fetch
-        setImageLoaded(false); // Reset image loading state
         const response = await axios.get(`https://proto-backend.vercel.app/robots/name/${robotName}`);
         setRobotData(response.data);
       } catch (err) {
         setError(err);
       } finally {
-        setLoading(false); // Set loading to false after data fetch
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [robotName]);
 
-  if (loading || !imageLoaded) return <Spinner />;
+  if (loading) return <Spinner />;
   if (error) return (
     <Alert status="error">
       <AlertIcon />
@@ -99,7 +96,6 @@ const RobotDetails = ({ robotName }) => {
             h="70vh"
             w="100%"
             borderRadius="md"
-            onLoad={() => setImageLoaded(true)}  // Set imageLoaded to true when image is loaded
           />
         </ScaleFade>
         <CardBody>
@@ -128,4 +124,4 @@ const RobotDetails = ({ robotName }) => {
   );
 };
 
-export default RobotDetails;
+export default RobotDetails;  
